5ec194ab072078cab5199c7e8021b5f30b9fb51d
grapedge <grapedge@gmail.com>
2021-06-30 02:28:58 +0800

    feat: 完成成绩查询提醒功能

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,84 @@
 import { createFetchWithCookies } from './libs/fetch';
 import { CookieJar } from 'tough-cookie';
+import yamljs from 'yamljs';
 import { sduAuth } from './auth';
 import { getAllScores } from './score';
+import path from 'path';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+import { delay } from './libs/delay';
+import { existsSync, readFileSync, writeFileSync } from 'fs';
+import { ScoreMailOptions, sendScoreMail } from './send-score-mail';
+
+type AppOptions = {
+  casId: string;
+  password: string;
+  semester: string;
+  interval: number;
+  smtp: SMTPTransport.Options;
+  mail: ScoreMailOptions;
+};
+
+async function createAuthFetch(
+  service: string,
+  casId: string,
+  password: string
+) {
+  const cookieJar = new CookieJar();
+  const fetch = createFetchWithCookies(cookieJar);
+  await sduAuth(fetch, service, casId, password);
+  return fetch;
+}
 
 async function main() {
-  const jar = new CookieJar();
-  const fetch = createFetchWithCookies(jar);
-  await sduAuth(
-    fetch,
-    'http://bkjws.sdu.edu.cn/f/j_spring_security_thauth_roaming_entry',
-    '222',
-    '222'
+  const options: AppOptions = yamljs.load(
+    path.resolve(__dirname, '..', 'config.yaml')
   );
-  const scores = await getAllScores(fetch);
-  console.log(scores);
+
+  // 已发布成绩的课程 ID
+  let postedCourses: string[] = [];
+  const postedJsonPath = path.resolve(__dirname, '..', 'posted.json');
+  if (existsSync(postedJsonPath)) {
+    postedCourses = JSON.parse(
+      readFileSync(path.resolve(__dirname, '..', 'posted.json')).toString()
+    );
+  }
+
+  while (true) {
+    // 登录系统
+    const fetch = await createAuthFetch(
+      'http://bkjws.sdu.edu.cn/f/j_spring_security_thauth_roaming_entry',
+      options.casId,
+      options.password
+    );
+    await delay(1000);
+
+    // 轮询获取数据
+    try {
+      while (true) {
+        const scores = await getAllScores(fetch);
+        // 获取本学期的课程
+        const currentScores = scores.filter(
+          (score) => score.semester === options.semester
+        );
+        // 成绩有变动
+        if (currentScores.length !== postedCourses.length) {
+          const changes = currentScores.filter(
+            (score) => !postedCourses.includes(score.courseId)
+          );
+          await Promise.all(
+            changes.map((score) =>
+              sendScoreMail(options.smtp, options.mail, score)
+            )
+          );
+          postedCourses = currentScores.map((score) => score.courseId);
+          writeFileSync(postedJsonPath, JSON.stringify(postedCourses));
+        }
+        await delay(options.interval);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 main();
---
+++ src/send-score-mail.ts
@@ -0,0 +1,94 @@
+import nodemailer from 'nodemailer';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+import { Score } from './score';
+
+export type ScoreMailOptions = {
+  from: string;
+  to: string;
+};
+
+const createHtml = (score: Score) => `
+<h3>【${score.courseName}】成绩发布</h3>
+<table>
+  <tr>
+    <td>
+      课程名
+    </td>
+    <td>
+      ${score.courseName}
+    </td>
+  </tr>
+  <tr>
+    <td>
+      学年学期
+    </td>
+    <td>
+      ${score.semester}
+    </td>
+  </tr>
+  <tr>
+    <td>
+      考试时间
+    </td>
+    <td>
+      ${score.examTime.toLocaleString()}
+    </td>
+  </tr>
+  <tr>
+    <td>
+      绩点
+    </td>
+    <td>
+      ${score.gradePoint}
+    </td>
+  </tr>
+  <tr>
+    <td>
+      学分
+    </td>
+    <td>
+      ${score.credits}
+    </td>
+  </tr>
+  <tr>
+    <td>
+      考试成绩
+    </td>
+    <td>
+      ${score.score}
+    </td>
+  </tr>
+  <tr>
+    <td>
+      成绩等级
+    </td>
+    <td>
+      ${score.level}
+    </td>
+  </tr>
+  <tr>
+    <td>
+      发布时间
+    </td>
+    <td>
+      ${new Date().toLocaleString()}
+    </td>
+  </tr>
+</table>
+`;
+
+export async function sendScoreMail(
+  smtp: SMTPTransport.Options,
+  mail: ScoreMailOptions,
+  score: Score
+) {
+  console.log(`[${score.courseName}]成绩已出，开始发送邮件提醒`);
+  const transporter = nodemailer.createTransport(smtp);
+  await transporter.sendMail({
+    from: mail.from,
+    to: mail.to,
+    subject: '成绩查询小助手',
+    html: createHtml(score),
+  });
+  console.log(`[${score.courseName}]成绩发送完毕`);
+}
